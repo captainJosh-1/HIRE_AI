68ab5ef66808b14cea0ea1417c8047cace0eb7dc
captainJosh-1 <chetanoffi.23@gmail.com>
2026-08-15 17:52:48 +0530

    feat: implement Prisma client setup and database connection in server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
-import app from "./app"
+import app from "./app.js"
 
+import prisma  from "./lib/prisma.js";
 dotenv.config();
 
 
@@ -11,3 +12,19 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT , () => {
     console.log(`sever running on port ${PORT}`);
 });
+
+async function startServer() {
+  try {
+    const users = await prisma.user.findMany();
+
+    console.log("Users from database:", users);
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Database connection failed:", error);
+  }
+}
+
+startServer();
