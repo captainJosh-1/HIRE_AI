66484853c7c620e3728985d2307ac3ff94332b59
captainJosh-1 <chetanoffi.23@gmail.com>
2026-09-01 11:08:37 +0530

    refactor: restructure project architecture

--- backend/src/modules/jobSeeker/profile/jobSeeker.controller.ts
+++ backend/src/modules/jobSeeker/profile/jobSeeker.controller.ts
@@ -28,39 +28,4 @@ const updateProfileController = asyncHandler(async(req:Request , res:Response)=>
     return res.status(200).json(new ApiResponse(200 , updatedjobSeekerProfile ,"Profile is updated successfully"))
 })
 
-
-// const addSkillController = asyncHandler(async( req:Request , res:Response)=>{
-//     const { name } = req.body;
-//     const { userId } = req.user!
-
-//     const skillsAdded = await addSkills(
-//         userId,
-//         name,
-//     );
-
-//     return res.status(200).json(new ApiResponse(200 , skillsAdded, "Skills are added successfully"))
-// })
-
-// const getMySkillController = asyncHandler(async(req:Request , res:Response)=>{
-//     const {userId} = req.user!
-
-//     const MySkills = await getMySkills(
-//         userId
-//     )
-
-//     return res.status(200).json(new ApiResponse(200 , MySkills,"User skills are fechted"))
-// })
-
-// const deleteMySkills = asyncHandler(async(req:Request , res:Response)=>{
-//     const {userId} =req.user!
-//     const {skillId} = req.params
-
-//     const deleteSkill = await deleteMyskills(
-//         userId,
-//         Number(skillId)
-//     )
-
-//     return res.status(200).json(new ApiResponse(200 , deleteSkill , "Skill is delted"))
-// })
-
-export { getMyProfileController , updateProfileController ,addSkillController , getMySkillController, deleteMySkills}
+export { getMyProfileController , updateProfileController}
--- backend/src/routes/auth.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { login, register  } from "../controllers/auth.controller.js";
-import { authMiddleware } from "../middleware/auth.middleware.js";
-const router = Router();
-
-router.post("/register" , register);
-router.post("/login",login);
-export {router};
